f2624402b96056a124d87bd33df8f2d1b303dffa
alaresof <alfaresof@gmail.com>
2023-02-19 21:43:30 +0300

    make last report

---
+++ lib/view/last_reports/last_report.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LastReports extends StatelessWidget {
+  const LastReports({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('last Reports'),
+    );
+  }
+}
