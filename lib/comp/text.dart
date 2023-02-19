1d8104d7e2f1bf092435de2f0710d5bb87e16dd8
alaresof <alfaresof@gmail.com>
2023-02-19 21:44:08 +0300

    text assets

---
+++ lib/comp/text.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Text text(String text , double size ,FontWeight weight,Color color) => Text(
+  text,
+  style: GoogleFonts.inder(
+    fontSize: size,
+    fontWeight: weight,
+    color: color
+  )
+);
