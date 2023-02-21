a3bdf20728117832e432f8b75bf4e8aa72948ab8
alaresof <alfaresof@gmail.com>
2023-02-21 23:44:30 +0300

    set up firebase , change routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:imata/bloc/register/register_cubit.dart';
+import 'package:imata/config/router.dart';
+import 'package:imata/repo/auth_repo.dart';
 import 'package:imata/view/home/home_page.dart';
 import 'package:imata/view/login/login.dart';
 import 'package:imata/view/register/register.dart';
 import 'package:imata/view/thank/thank.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,18 +21,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider(
+      create: (context) => AuthRepositry(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: Register.routename,
+
+        // routes: {
+        //   '/': (context) => const HomePage(),
+        //   '/login': (context) => const Login(),
+        //   '/register': (context) =>  Register(),
+        //   '/thank': (context) => const Thank(),
+        // },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        '/login': (context) => const Login(),
-        '/register':(context) =>  const Register(),
-        '/thank': (context) => const Thank(),
-      },
     );
   }
 }
