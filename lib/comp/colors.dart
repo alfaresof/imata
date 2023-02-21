91dc665137242ed9e3f9574d9d16f6a65dda4595
alaresof <alfaresof@gmail.com>
2023-02-21 23:44:03 +0300

    add color

--- lib/comp/colors.dart
+++ lib/comp/colors.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 
 Color lightGreen = const Color.fromRGBO(34, 141, 45, 0.52);
+Color lightyGreen = const Color.fromRGBO(163, 208, 168, 0.28);
+
 Color lightGrey = const Color.fromRGBO(111, 111, 112, 1);
 Color darkGreen = const Color.fromRGBO(163, 208, 168, 1);
 Color blackGreen = const Color.fromRGBO(116, 149, 120, 1);
